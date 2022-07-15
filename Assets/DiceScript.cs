0fe888dd14d43d76727b1df5ad5bd1b4b52ded00
bananaizg0d <101892890+bananaizg0d@users.noreply.github.com>
2022-07-15 23:19:20 +0300

    logic

--- Assets/DiceScript.cs
+++ Assets/DiceScript.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class DiceScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool dicable = true;
+    public bool diceHitable;
+
+    public bool diceSucceded;
+
+    public float diceCD = 5;
+    public float diceStartZone = 1.5f;
+    public float diceHitZone = 0.5f;
+    public float diceCycle;
+
+    public float powerRandomizer;
+
+    private void Start()
     {
-        
+        diceCycle = diceCD + diceHitZone + diceStartZone;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (dicable)
+        {
+            InvokeRepeating(nameof(DiceStart), diceCD, diceCycle);
+        }
+
+        InvokeRepeating(nameof(CheckSuccess), diceCycle, diceCycle);
+
+        if (diceHitable && Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("success");
+            diceSucceded = true;
+        }
+        //else if ()
+        //{
+
+        //}
+    }
+
+    public void LevelStart() => dicable = true;
+
+    public void DiceStart()
+    {
+        Invoke(nameof(DiceHitZone), diceStartZone);
+        Debug.Log("a");
+    }
+
+    public void DiceHitZone()
+    {
+        Invoke(nameof(DiceEnd), diceHitZone);
+        diceHitable = true;
+        Debug.Log("b");
+    }
+
+    public void DiceEnd()
+    {
+        diceHitable = false;
+        Debug.Log("c");
+    }
+
+    public void CheckSuccess()
+    {
+        if (diceSucceded)
+        {
+            Debug.Log("success");
+        }
+        else if (!diceSucceded)
+        {
+            Debug.Log("failed");
+        }
+        diceSucceded = false;
     }
 }
