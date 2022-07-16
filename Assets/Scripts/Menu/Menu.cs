04a520b48cc2230d314526fc4182bbc811ce22c9
IAmNotGameDev <109346238+IAmNotGameDev@users.noreply.github.com>
2022-07-16 18:14:37 +0600

    menu 0.5

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Control()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Control");
+    }
+
+
+    public void Credits()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Credits");
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
