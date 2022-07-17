e7a4a91987c9824f856c50a80b0b184a4e28d3ec
yanerobot <screwyuguys@gmail.com>
2022-07-17 16:56:13 +0300

    s

--- Assets/_Scripts/TimerScript.cs
+++ Assets/_Scripts/TimerScript.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
+using TMPro;
+using System.Text;
 
 public class TimerScript : MonoBehaviour
 {
+    TextMeshProUGUI timerText;
+    float currentSeconds;
+    int currentMins;
+    bool isStarted;
 
+    StringBuilder sb;
+
+    string finalTimerValue;
+
+    void Start()
+    {
+        currentSeconds = 0;
+        currentMins = 0;
+        sb = new StringBuilder();
+    }
+    void Update()
+    {
+        if (!isStarted)
+            return;
+        if (Time.timeScale == 0)
+            return;
+
+        sb.Clear();
+
+        currentSeconds += Time.unscaledDeltaTime;
+
+        finalTimerValue = (currentSeconds % 60).ToString("00");
+    }
 }
